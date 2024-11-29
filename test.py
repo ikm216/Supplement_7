628516aa389e56960d6bd3c31893fd128340c7c1
ikm216 <ikmclassof22@gmail.com>
2024-11-29 15:55:01 -0500

    test: added test to plot 200 points

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-def test():
-    pass
+import numpy as np
 
-test()
+def test_should_return_200_plots():
+    points = np.random.normal(0, 1, 200)
+    assert len(points) == 200
+    assert np.isclose(np.mean(points), 0, atol = 0.5)
+    assert np.isclose(np.std(points), 1, atol = 0.5)
+
+    plot_200()
