f928f921ed1fd0e9217d923b44e8092d7456ba7b
ikm216 <ikmclassof22@gmail.com>
2024-11-29 17:00:44 -0500

    docs: added documentation to the function plot_200

--- test.py
+++ test.py
@@ -2,7 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_200():
-    points = np.random.normal(0, 1, 200)
+    """
+    Plots 200 points from a standard normal distribution.
+
+    This function generates 200 random points from a standard normal distribution
+    (mean = 0, standard deviation = 1) and plots them as a scatter plot. The x-axis
+    represents the index of the points (from 0 to 199), and the y-axis represents
+    the values of the sampled points.
+
+    Returns:
+        Display the plots on a grid
+    """
+    points = np.random.normal(0, 1, 200) #np.random.normal: generates random numbers from a normal distrubution
     plt.scatter(range(200), points)
     plt.title("Plots")
     plt.xlabel("Index")
@@ -13,7 +24,9 @@ def plot_200():
 def test_should_return_200_plots():
     points = np.random.normal(0, 1, 200)
     assert len(points) == 200
+    # np.isclose: checks if the numbers are equal
     assert np.isclose(np.mean(points), 0, atol = 0.5)
     assert np.isclose(np.std(points), 1, atol = 0.5)
 
     plot_200()
+
