9ef6f2e4729af7e02beaef6963a691ba1d9fe9b7
ikm216 <ikmclassof22@gmail.com>
2024-11-29 17:15:02 -0500

    test: added test to plot a line

--- test.py
+++ test.py
@@ -21,6 +21,19 @@ def plot_200():
     plt.grid(True)
     plt.show()
 
+def plot_line(y_intercept, slope, low_x, up_x):
+    x = np.linspace(low_x, up_x, 500)
+    y = slope * x + y_intercept
+
+    plt.plot(x, y)
+    plt.xlim(low_x, up_x)
+    plt.title("Plot Line")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 def test_should_return_200_plots():
     points = np.random.normal(0, 1, 200)
     assert len(points) == 200
@@ -29,4 +42,18 @@ def test_should_return_200_plots():
     assert np.isclose(np.std(points), 1, atol = 0.5)
 
     plot_200()
+
+def test_should_return_plot_line():
+    y_intercept = 5
+    slope = 2
+    low_x = -7
+    up_x = 7
+
+    x = np.linspace(low_x, up_x, 5)
+    y = slope * x + y_intercept
+
+    plot_graph = [slope * xi + y_intercept for xi in x]
+    assert np.allclose(y, plot_graph)
+
+    plot_line(y_intercept, slope, low_x, up_x)
 
